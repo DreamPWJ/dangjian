ab9bf1000114a31b21df682c9b709ab4563ab13e
406798106@qq.com <406798106@qq.com>
2017-10-08 20:56:59 +0800

    智慧党建TV版

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  videoUrl: string = "http://jxdj1.rzzyfw.com/upload/dy/yyzls/1.mp4";
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  //直播
+  liveStreaming() {
+    this.videoUrl = "http://u166.auto.s.wanglitiaoyi.com/live/3693838317.m3u8"
+  }
+
+  //主页
+  liveHome() {
+    this.videoUrl = "http://jxdj1.rzzyfw.com/upload/dy/yyzls/1.mp4"
+  }
 }
