c8afac4677f9dd4d7edb722ab4407a6731d2507c
406798106@qq.com <406798106@qq.com>
2017-10-09 11:09:31 +0800

    智慧党建TV版

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,18 +7,31 @@ import {NavController} from 'ionic-angular';
 })
 export class HomePage {
   videoUrl: string = "http://jxdj1.rzzyfw.com/upload/dy/yyzls/1.mp4";
+  typeFlag: number = 0;
 
   constructor(public navCtrl: NavController) {
 
   }
 
-  //直播
-  liveStreaming() {
-    this.videoUrl = "http://u166.auto.s.wanglitiaoyi.com/live/3693838317.m3u8"
+  //类型切换
+  typeSwitch(type) {
+    if (type == 1) {  //智慧党建
+      this.typeFlag = 1
+    } else if (type == 2) { //三务公开
+      this.typeFlag = 2
+    } else {//主页
+      this.typeFlag = 0
+    }
   }
 
-  //主页
-  liveHome() {
-    this.videoUrl = "http://jxdj1.rzzyfw.com/upload/dy/yyzls/1.mp4"
+  //视频切换
+  liveStreaming(type) {
+    if (type == 0) {  //主页
+      this.typeFlag = 0
+      this.videoUrl = "http://jxdj1.rzzyfw.com/upload/dy/yyzls/1.mp4"
+    } else if (type == 1) { //直播
+      this.videoUrl = "http://u166.auto.s.wanglitiaoyi.com/live/3693838317.m3u8"
+    }
   }
+
 }
