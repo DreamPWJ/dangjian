02de0f7c6e8f94f511fa774028d695e7a0fb1603
406798106@qq.com <406798106@qq.com>
2017-10-12 17:46:39 +0800

    智慧党建TV版

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -19,6 +19,9 @@ export class HomePage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomePage');
     this.typeFlag = Number(localStorage.getItem("topTypeFlag")) || 1;
+    if (this.typeFlag == 6) { //红色影院
+      this.getFilms();
+    }
   }
 
   //类型切换
@@ -37,11 +40,16 @@ export class HomePage {
       this.typeFlag = 5
     } else if (type == 6) { //红色影院
       this.typeFlag = 6
-      this.appService.httpGet("https://api.douban.com/v2/movie/search", {tag: "抗战",count:13},  (data) =>{
-        console.log(data);
-        this.redFilms=data.subjects;
-      })
+      this.getFilms();
     }
+  }
+
+  //获取电影数据
+  getFilms() {
+    this.appService.httpGet("https://api.douban.com/v2/movie/search", {tag: "抗战", count: 13}, (data) => {
+      console.log(data);
+      this.redFilms = data.subjects;
+    })
   }
 
   //视频切换
@@ -59,16 +67,16 @@ export class HomePage {
   searchFilm(ev: any) {
     // Reset items back to all of the items
     // set val to the value of the searchbar
-/*    let val = ev.target.value;*/
+    /*    let val = ev.target.value;*/
     // if the value is an empty string don't filter the items
-/*    if (val && val.trim() != '') {
-      this.redFilms = this.redFilms.filter((item) => {
-        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
-      })
-    }*/
+    /*    if (val && val.trim() != '') {
+          this.redFilms = this.redFilms.filter((item) => {
+            return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+          })
+        }*/
   }
-  pushFilmPage(id){
 
+  pushFilmPage(id) {
     this.navCtrl.push('FilmPage', {
       id: id,
     });
