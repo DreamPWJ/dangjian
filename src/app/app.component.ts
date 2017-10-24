ab3d0287a9eef8f4c59c5bf5118644e2cac587fa
406798106@qq.com <406798106@qq.com>
2017-10-24 15:12:35 +0800

    智慧党建TV版

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,13 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+      if(statusBar){
+        statusBar.styleDefault();
+      }
+      if(splashScreen){
+        splashScreen.hide();
+      }
+
 
     });
   }
