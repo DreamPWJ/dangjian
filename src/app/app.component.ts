ae408b52ae40568a650693ec9fd8230332499bb5
406798106@qq.com <406798106@qq.com>
2017-10-31 16:09:02 +0800

    智慧党建TV版

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {Component} from '@angular/core';
+import {App, IonicApp, Keyboard, NavController, Platform} from 'ionic-angular';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
+import {AppService} from "./app.service";
+import {HomePage} from "../pages/home/home";
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
+  backButtonPressed: boolean = false;  //用于判断返回键是否触发
+  private nav: NavController;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(public platform: Platform,  statusBar: StatusBar,  splashScreen: SplashScreen, private app: App, private ionicApp: IonicApp, public appService:AppService, private keyboard: Keyboard) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -20,8 +24,40 @@ export class MyApp {
       if(splashScreen){
         splashScreen.hide();
       }
-
+      this.nav = this.app.getActiveNav();
+      this.registerBackButtonAction();//注册返回按键事件
 
     });
+  }
+
+  registerBackButtonAction() {
+    this.platform.registerBackButtonAction(() => {
+      if (this.keyboard.isOpen()) {//如果键盘开启则隐藏键盘
+        this.keyboard.close();
+        return;
+      }
+      //如果想点击返回按钮隐藏toast或loading或Overlay就把下面加上
+      // this.ionicApp._toastPortal.getActive() || this.ionicApp._loadingPortal.getActive() || this.ionicApp._overlayPortal.getActive()
+      let activePortal = this.ionicApp._modalPortal.getActive();
+      if (activePortal) {
+        activePortal.dismiss().catch(() => {
+        });
+        activePortal.onDidDismiss(() => {
+        });
+        return;
+      }
+      return this.nav.canGoBack() ? this.nav.pop() : this.showExit()
+    }, 1);
+  }
+
+  //双击退出提示框
+  showExit() {
+    if (this.backButtonPressed) { //当触发标志为true时，即2秒内双击返回按键则退出APP
+      this.platform.exitApp();
+    } else {
+      this.appService.toast("再按一次返回退出智慧党建")
+      this.backButtonPressed = true;
+      setTimeout(() => this.backButtonPressed = false, 2000);//2秒内没有再次点击返回则将触发标志标记为false
+    }
   }
 }
